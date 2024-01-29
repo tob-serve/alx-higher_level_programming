feefc683a3dabd98ce513b9ec7cec02d73f4ef5c
leinad <leinaddaniel3100@gmail.com>
2024-01-29 21:42:35 +0000

    last update

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,4 +5,4 @@ word_last_2 = f"{word[-2:]}"
 middle_word = f"{word[1:-1]}"
 print("First 3 letters: %s" % word_first_3
 print("Last 2 letters: {}".format(word_last_2))
-print(f"Middle word: {middle_word")
+print(f"Middle word: {middle_word}")
