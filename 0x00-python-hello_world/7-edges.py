56bf2ac55837ff9e3f25cde9212da06a92e329e3
leinad <leinaddaniel3100@gmail.com>
2024-01-29 21:46:33 +0000

    checking updates

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = f"{word[:3]}"
 word_last_2 = f"{word[-2:]}"
 middle_word = f"{word[1:-1]}"
-print("First 3 letters: %s" % word_first_3
-print("Last 2 letters: {}".format(word_last_2))
+print("First 3 letters: {word_first_3}")
+print("Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
