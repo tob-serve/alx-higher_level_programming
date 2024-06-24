6b8bafd8fdddbfe3d8a7f471ef2ec0dcbbd84f19
tob-serve <leinaddaniel3100@gmail.com>
2024-06-24 15:25:31 -0700

    i did it alx way

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const argc = process.argv.length - 2;
-if (argc == 0) {
+if (process.argv.length == 2) {
         console.log('No argument');
-} else if (argc == 1) {
+} else if (process.argv.length == 3) {
         console.log('Argument found');
 } else {
         console.log('Argument found');
