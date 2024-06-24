d78080f42828c33ac08b0bb2008aea117b65c45a
tob-serve <leinaddaniel3100@gmail.com>
2024-06-24 02:35:30 -0700

    checking changes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,6 +5,8 @@ let argc = argv.length - 2;
 
 if (argc == 0) {
         console.log('No argument')
-} else{
+} elif (argc == 1) {
+        console.log('Argument found')
+} else {
         console.log('Argument found')
 }
