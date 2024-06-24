ac6ff68abb45edb1f047b457e4a4e6af35cc221d
tob-serve <leinaddaniel3100@gmail.com>
2024-06-24 15:13:38 -0700

    checking changes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let argc = argv.length - 2;
 if (argc == 0) {
-        console.log('No argument')
+        console.log('No argument');
 } elif (argc == 1) {
-        console.log('Argument found')
+        console.log('Argument found');
 } else {
-        console.log('Argument found')
+        console.log('Argument found');
 }
