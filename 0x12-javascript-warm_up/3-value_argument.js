4990591afdf6e5012e3785cfa56b84f45e249dca
tob-serve <leinaddaniel3100@gmail.com>
2024-06-25 11:02:33 -0700

    checking changes

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (argv[2] === undefined) {
-	console.log('No argument');
-} else {
-	argV = argv[3]
-	console.log(`${argV}`);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No arguments' : process.argv[3]);
