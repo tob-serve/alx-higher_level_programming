2cc36a943cbd4c97754f11586e530716ff8fbee9
tob-serve <leinaddaniel3100@gmail.com>
2024-06-24 18:09:15 -0700

    ahhh finally this is it

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argCount = process.argv.length;
-console.log(argCount === 2 ? 'No argument' : argCount === 3 ? 'Argument found' : 'Arguments found')
+console.log(argCount === 2 ? 'No argument' : argCount === 3 ? 'Argument found' : 'Arguments found');
