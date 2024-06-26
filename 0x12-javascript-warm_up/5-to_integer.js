fa039846d295614cf14ec6608cdca9f71a0eefde
tob-serve <leinaddaniel3100@gmail.com>
2024-06-25 17:49:17 -0700

    am done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-function isNumber(value) {
-	return !isNaN(Number(+value));
-}
-console.log(isNumber(process.argv[2]) ? `My number: ${process.argv[2]}` : 'Not a number');
+console.log(!isNaN(+process.argv[2]) ? `My number: ${process.argv[2]}` : 'Not a number');
