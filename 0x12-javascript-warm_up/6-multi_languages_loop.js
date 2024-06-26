c3c0d412a9d2337f24d51beadaab74bc209b858b
tob-serve <leinaddaniel3100@gmail.com>
2024-06-26 11:52:36 -0700

    hmmm

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let message = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let message = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let index =  0;
 while (index !== 3) {
 	console.log(message[index]);
