438bcc806c81acea110936dd4bfdcf2bf1b7160e
tob-serve <leinaddaniel3100@gmail.com>
2024-06-28 18:26:47 -0700

    cheking chanes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-const text = ['C is fun'];
-while (!isNaN(+process.argv[2]) === 'false') {
-  console.log('Missing number of occurrences');
-  break;
-}
-while (!isNaN(+process.argv[2])) {
-  const times = +process.argv[2];
-  console.log(Array(times).fill(text).join('\n'));
-  break;
+let i;
+const times = process.argv[2];
+const number = Number(times);
+
+if (isNaN(number)) {
+  console.log('Missing number of occurrances');
+} else {
+  while (i < number) {
+    console.log('C is fun');
+    i++;
+  }
 }
